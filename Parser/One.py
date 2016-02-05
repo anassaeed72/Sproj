8d2e77a3358195e8db180afda2fe55534ae2e4eb
xen <xen@debian.debianxen>
2016-02-05 18:42:52 -0500

    Intial Generator

---
+++ Parser/One.py
@@ -0,0 +1,9 @@
+from  OneOperationGenerator import OneOperationGenerator
+
+
+one = OneOperationGenerator()
+one.operationName = "test"
+one.fileName = "test.xml";
+one.operationInputFile = "tempinputfile";
+one.OperationPath = "OperationPath"
+one.writeToFile()
