e06f32a3fd3177e5f1d74677747d0f41657a0f4d
Anas Saeed <anassaeed72@gmail.com>
2016-02-06 19:55:09 +0500

    Cybox Integration in One Operation

--- Parser/OneOperation.py
+++ Parser/OneOperation.py
@@ -1,5 +1,10 @@
 import os
 import sys
+def cybox(cyboxXmlValue):
+	print "In cybox " + cyboxXmlValue
+
+def ifCondition():
+	print "In If Condition"
 if len(sys.argv) <2:
 	print('Arguments not given')
 	sys.exit()
@@ -10,6 +15,15 @@ xmldoc = minidom.parse(sys.argv[1])
 operationName = xmldoc.getElementsByTagName('operationName')
 operationNameValue = operationName[0].attributes['myvalue'].value
 
+if operationNameValue == "cybox":
+	cyboxXml = xmldoc.getElementsByTagName('cyboxXml')
+	cyboxXmlValue = cyboxXml[0].attributes['myvalue'].value
+	cybox(cyboxXmlValue)
+	sys.exit(1)
+
+if operationNameValue =="IfCondition":
+	ifCondition()
+	
 
 operationInputFile = xmldoc.getElementsByTagName('operationInputFile')
 operationInputFilevalue = operationInputFile[0].attributes['myvalue'].value
@@ -21,4 +35,6 @@ operationPathValue = operationPath[0].attributes['myvalue'].value
 
 commandToExecute = "python " + operationPathValue + "/" + operationNameValue + ".py " + operationInputFilevalue
 print "Command : "+operationNameValue
-os.system(commandToExecute)
+print commandToExecute
+# os.system(commandToExecute)
+
