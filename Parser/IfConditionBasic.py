260c2dd569ef74913da711eafc2079a3f511561e
xen <xen@debian.debianxen>
2016-02-18 16:08:31 -0500

    Blacklist not working

--- Parser/IfConditionBasic.py
+++ Parser/IfConditionBasic.py
@@ -2,7 +2,7 @@ import os
 import sys
 from lxml import etree
 import xml.etree.ElementTree
-
+import re
 if len(sys.argv) <2:
 	print('Arguments not given')
 	sys.exit()
@@ -17,18 +17,35 @@ rightValue = right[0].attributes['myvalue'].value
 operator = xmldoc.getElementsByTagName('operator')
 operatorValue = operator[0].attributes['myvalue'].value
 
+print leftValue
+print rightValue
+print operatorValue
 if operatorValue == "equals":
 	if leftValue == rightValue:
-		print "true"
+		print "trueequals"
 	else:
 		print "false"
 elif operatorValue == "greater":
 	if leftValue > rightValue:
-		print "true"
+		print "truegreater"
 	else:
 		print "false"
 elif operatorValue == "smaller":
 	if leftValue < rightValue:
-		print "true"
+		print "truesmaller"
 	else:
 		print "false"
+elif operatorValue =="contains":
+	with open(rightValue) as fileObject:
+		while True:
+			line = fileObject.readLine()
+			if not line:
+				break
+			regex = '*'+leftValue
+			matchObj = re.match(regex,line)
+			if matchObj:
+				print "true1"
+				break
+
+	print "false"
+
