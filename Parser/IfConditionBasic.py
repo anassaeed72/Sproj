7addad591c8104d3789ae0d6a3f600a2fd4f5535
xen <xen@debian.debianxen>
2016-02-24 18:09:29 -0500

    If with Mongodb

--- Parser/IfConditionBasic.py
+++ Parser/IfConditionBasic.py
@@ -2,6 +2,8 @@ import os
 import sys
 from lxml import etree
 import xml.etree.ElementTree
+import re
+from pymongo import MongoClient
 
 if len(sys.argv) <2:
 	print('Arguments not given')
@@ -10,25 +12,25 @@ if len(sys.argv) <2:
 from xml.dom import minidom
 xmldoc = minidom.parse(sys.argv[1])
 
-left = xmldoc.getElementsByTagName('left')
+left = xmldoc.getElementsByTagName('key')
 leftValue = left[0].attributes['myvalue'].value
-right = xmldoc.getElementsByTagName('right')
+right = xmldoc.getElementsByTagName('value')
 rightValue = right[0].attributes['myvalue'].value
-operator = xmldoc.getElementsByTagName('operator')
-operatorValue = operator[0].attributes['myvalue'].value
 
-if operatorValue == "equals":
-	if leftValue == rightValue:
-		print "true"
-	else:
-		print "false"
-elif operatorValue == "greater":
-	if leftValue > rightValue:
-		print "true"
-	else:
-		print "false"
-elif operatorValue == "smaller":
-	if leftValue < rightValue:
-		print "true"
-	else:
-		print "false"
+print leftValue
+print rightValue
+client = MongoClient()
+db = client.test
+
+
+key_ =leftValue# sys.argv[2] # key is col and value is 127.0.0.1
+value =rightValue# sys.argv[3]
+
+key_val = {}
+key_val[key_] = value
+
+cursor = db.testcollection.find(key_val)
+
+for document in cursor:
+	print ' '.join('| {} : {} |'.format(key, val) for key, val in sorted(document.items()))
+print "true"
