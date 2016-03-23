44acf6a39225151c6d69016aa826de07710d3a5b
Anas Saeed <anassaeed@Anass-iMac.local>
2016-03-24 02:43:18 +0500

    Resolved input issues

---
+++ Primitives/Proceses/PsList/Constants.py
@@ -0,0 +1,13 @@
+from enum import Enum
+class ConstantsClass:
+	# pathValue="/home/xen/Documents/Sproj/"
+	pathValue="/Users/Anas_Saeed/Dropbox/8th\ Semester/Sproj/Sproj/"
+	# PrintLevel = enum(Command=True, IfConditionAnswer=True, IfConditionCondition=True,BaseClass =True)
+
+class PrintLevel(Enum):
+	Command=True
+	IfConditionAnswer=True
+	IfConditionCondition=True
+	BaseClass =True
+	Error = True
+	RawOuput = True
